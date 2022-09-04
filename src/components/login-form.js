597b5820b69ccafa2368c41efb8858bff89c7147
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-09-04 11:38:15 -0500

    Styling login-form, document. Still working on Navbar

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -4,20 +4,20 @@ export default class LoginForm extends Component {
     render() {
       return (
         //Can only return 1 result, so have to wrap everything inside a div
-        <div className="container" style={{border: "solid 3px"}}>
+        <div className="container" id="loginArea">
             {/* Label in h3 */}
             <h3>Log In</h3>
             {/* it's login FORM, so putting inside form tags */}
                 <form>
                     {/* username input field, inside a div so they don't run side by side with the password field*/}
-                    <div className="mb-3">
-                        <label for="username">Username:</label>
-                        <input type="text" id="username" name="username"></input>
+                    <div className="mb-2">
+                        <label htmlFor="username" className="form-label">Username:</label>
+                        <input type="text" id="username" name="username" className="form-control"></input>
                     </div>
                     {/* password input field, as above */}
-                    <div className="mb-3">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password"></input>
+                    <div className="mb-2">
+                        <label htmlFor="password" className="form-label">Password:</label>
+                        <input type="password" id="password" name="password" className="form-control"></input>
                     </div>
                     {/* needs a submit button */}
                     <button type="submit" className="btn btn-primary" name="sumbit">Submit</button>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,16 +3,17 @@ import React, { Component } from "react";
 export default class Navigation extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container" id="navBar">
             {/* a bunch of links that go nowhere */}
                 <nav className="navbar">
+                    <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
                     
 
 
 
 
-
+                    </div>
                 </nav>
             </div>
         )
