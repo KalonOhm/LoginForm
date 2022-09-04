7540edbcf05388296f99e2fa9214865735de9d44
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-09-04 02:48:14 -0500

    Login Form appears to be working

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default class LoginForm extends React.Component {
+export default class LoginForm extends Component {
     render() {
-        return React.createElement('button', {class: 'btn btn-primary'}, 'Like');
+      return (
+        //Can only return 1 result, so have to wrap everything inside a div
+        <div className="container" style={{border: "solid 3px"}}>
+            {/* Label in h3 */}
+            <h3>Log In</h3>
+            {/* it's login FORM, so putting inside form tags */}
+                <form>
+                    {/* username input field, inside a div so they don't run side by side with the password field*/}
+                    <div className="mb-3">
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username"></input>
+                    </div>
+                    {/* password input field, as above */}
+                    <div className="mb-3">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password"></input>
+                    </div>
+                    {/* needs a submit button */}
+                    <button type="submit" name="sumbit">Submit</button>
+                </form>
+        </div>
+      );
     }
 }
---
+++ src/components/navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class Navigation extends Component {
+    render() {
+        return React.createElement('button', {class: 'btn btn-primary'}, 'Like');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
